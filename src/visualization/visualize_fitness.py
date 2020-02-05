d28eb61c71945f9c882510990f2454537532ae1d
ramanganti <rg468@cam.ac.uk>
2020-02-05 13:08:39 -0500

    "added class ProcessProtocolData for use in 0.1-rganti-visualize-protocol-curves"

--- src/visualization/visualize_fitness.py
+++ src/visualization/visualize_fitness.py
@@ -55,3 +55,46 @@ class InjectionKlDistance(Injection):
                 kl_distance.append(self.p_ini[i] * np.log(self.p_ini[i] / self.f[i]))
 
         return np.sum(kl_distance)
+
+
+class ProcessProtocolData(object):
+
+    def __init__(self, path, num_gcs=100.0):
+        self.sigma_1_range = np.sort(np.logspace(1.0, -1.0, num=15))[::-1]
+        self.fitness_array = []
+        self.kl1_array = []
+        self.kl2_matrix = []
+        self.mean_bnab_array = []
+        self.error_bnab_array = []
+        self.path = path
+        self.num_gcs = num_gcs
+
+    def load_arrays(self):
+        for sigma1 in self.sigma_1_range[:-3]:
+            print("Processing sigma_1 = {0}".format(sigma1))
+
+            p0 = []
+            total_bnabs = []
+
+            path = self.path + "Sigma_{0}/".format(round(sigma1, 2))
+            p0.append(np.loadtxt(path + "event_prob"))
+            total_bnabs.append(np.loadtxt(path + "total_bnabs"))
+
+            p0_mean = np.mean(p0, axis=0)
+            kl1 = InjectionKlDistance(p0_mean, sigma=sigma1)
+            self.kl1_array.append(kl1.compute_kl_distance())
+
+            self.fitness_array.append(kl1.f)
+            self.mean_bnab_array.append(np.array(total_bnabs) / self.num_gcs)
+
+            sigma2_range = np.loadtxt(path + "sigma2_range")
+
+            kl2_array = []
+            for sigma2 in sigma2_range:
+                p1 = []
+                p1.append(np.loadtxt(path + "n_ave") / np.sum(np.loadtxt(path + "n_ave")))
+                p1_mean = np.mean(p1, axis=0)
+                kl2 = InjectionKlDistance(p1_mean, sigma=sigma2)
+                kl2_array.append(kl2.compute_kl_distance())
+
+            self.kl2_matrix.append(kl2_array)
