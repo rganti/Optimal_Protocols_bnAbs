6b2acaca700e1ebc1bb24710a8e271dfd804c285
ramanganti <rg468@cam.ac.uk>
2020-02-09 15:30:38 -0500

    "Moved Simulation class to procedures.py. Changed code so parameters list can be passed containing all model parameters."

--- src/data/procedures.py
+++ src/data/procedures.py
@@ -1,9 +1,11 @@
 import os
+import time
 
 import numpy as np
 
-from src.data.gillespie_models import ModelMFPTTrajectories, ModelIndividualGC
-from src.data.simulation_setup import Simulation, BnabGillespie, BnabFiniteSizeEffects
+from src.data.gillespie_models import ModelMFPTTrajectories, ModelIndividualGC, Model
+from src.data.simulation_setup import BnabGillespie, BnabFiniteSizeEffects
+from src.data.ssc_setup import SSCLaunch
 from src.general.directory_handling import make_and_cd
 from src.data_process.simulation_post_process import print_info, GillespieGCExit
 from src.visualization.visualize_fitness import Injection
@@ -11,19 +13,19 @@ from src.visualization.visualize_fitness import Injection
 
 class Procedure(object):
 
-    def __init__(self, sigma_1=1.0, death_rate=0.1, fraction=7.0/8.0):
+    def __init__(self, parameters):
         self.n_initial = [20.0, 5.0, 0.0, 0.0, 0.0, 5.0, 20.0]
         self.p_ini = self.n_initial / np.sum(self.n_initial)
-        self.fraction = fraction
-        self.death_rate = death_rate
+
+        self.parameters = parameters
+
         self.home = os.getcwd()
-        self.sigma = [sigma_1, 0.4]
+        self.sigma = [parameters['sigma'], 0.4]
         self.total_bnabs = 0.0
 
     def run_cocktail(self):
         make_and_cd("Cocktail")
-        bnab_cocktail = Simulation(self.p_ini, sigma=self.sigma[1], fraction=self.fraction,
-                                   death_rate=self.death_rate)
+        bnab_cocktail = Simulation(self.p_ini, self.parameters)
         bnab_cocktail.run()
         os.chdir(self.home)
 
@@ -41,7 +43,7 @@ class Procedure(object):
         success_exit = np.loadtxt("../successful_exit")
         reps = len(success_exit)
 
-        bnab = Simulation(p_n, sigma=sigma, fraction=self.fraction, death_rate=self.death_rate)
+        bnab = Simulation(p_n, self.parameters)
         bnab.run(reps=int(reps))
 
     def run_sequential(self):
@@ -51,8 +53,7 @@ class Procedure(object):
         make_and_cd("Sigma_{0}".format(round(self.sigma[0], 2)))
         sigma_1_directory = os.getcwd()
 
-        bnab_sequential = Simulation(self.p_ini, sigma=self.sigma[0], fraction=self.fraction,
-                                     death_rate=self.death_rate)
+        bnab_sequential = Simulation(self.p_ini, self.parameters)
         bnab_sequential.run(reps=50)
 
         # seq_dir = os.getcwd()
@@ -72,24 +73,65 @@ class Procedure(object):
         self.run_sequential()
 
 
+class Simulation(object):
+
+    def __init__(self, p_ini, parameters, trajectories=False):
+        self.p_ini = p_ini
+        self.trajectories = trajectories
+        self.parameters = parameters
+
+        if len(self.p_ini) == 9:
+            self.gillespie_bnab = BnabFiniteSizeEffects(p_ini, parameters)
+        else:
+            self.gillespie_bnab = BnabGillespie(p_ini, parameters)
+
+        self.gillespie_bnab.define_tmat()
+
+    def run(self, reps=200):
+
+        if self.trajectories:
+            M = ModelMFPTTrajectories(p_ini=self.p_ini, vnames=self.gillespie_bnab.vars,
+                                      tmat=self.gillespie_bnab.tmat, propensity=self.gillespie_bnab.prop)
+        else:
+            M = Model(p_ini=self.p_ini, vnames=self.gillespie_bnab.vars, tmat=self.gillespie_bnab.tmat,
+                      propensity=self.gillespie_bnab.prop, n_stop=self.parameters['n_stop'])
+
+        np.savetxt("event_prob", self.p_ini, fmt='%f')
+        np.savetxt("fitness", self.gillespie_bnab.bnab.fitness.f, fmt='%f')
+        np.savetxt("sigma", [self.gillespie_bnab.bnab.fitness.sigma], fmt='%f')
+        np.savetxt("death_rate", [self.gillespie_bnab.bnab.mu_i0], fmt='%f')
+        np.savetxt("mutation_rate", [self.gillespie_bnab.bnab.mu_ij], fmt='%f')
+        np.savetxt("fraction", [self.gillespie_bnab.bnab.fraction], fmt='%f')
+
+        t0 = time.time()
+        M.run(tmax=1000, reps=reps)
+        print('total time: ', time.time() - t0)
+
+        print("Starting post-processing...")
+        post_process = GillespieGCExit(n_exit=self.parameters['n_stop'], num_odes=self.gillespie_bnab.len_ini)
+        post_process.populate_pn(index=1)
+
+        print("Printing SSC script w/ parameters")
+        bnab_ssc = SSCLaunch(self.p_ini, self.parameters)
+        bnab_ssc.generate_ssc_script("bnab_fitness")
+
+
 class ProcedureDelS1S2(Procedure):
 
-    def __init__(self, sigma_1=10.0, death_rate=0.1, fraction=7.0/8.0, trajectories=False):
-        Procedure.__init__(self, sigma_1=sigma_1, death_rate=death_rate, fraction=fraction)
-        self.sigma = np.logspace(np.log10(sigma_1), -1.0, num=10)
+    def __init__(self, parameters, trajectories=False):
+        Procedure.__init__(self, parameters)
+        self.sigma = np.logspace(np.log10(parameters['sigma']), -1.0, num=10)
         self.total_bnabs = []
         self.num_reps = 100
         self.trajectories = trajectories
-        self.n_initial = [20.0, 5.0, 0.0, 0.0, 0.0, 5.0, 20.0]
-        # self.n_initial = [20.0, 5.0, 0.0, 0.0, 0.0, 0.0, 0.0, 5.0, 20.0]
+        # self.n_initial = [20.0, 5.0, 0.0, 0.0, 0.0, 5.0, 20.0]
+        self.n_initial = parameters['n_initial']
         self.p_ini = self.n_initial / np.sum(self.n_initial)
-        self.n_stop = 200
+        self.parameters = parameters
 
     def run_prime(self):
-        bnab_sequential = Simulation(self.p_ini, sigma=self.sigma[0], fraction=self.fraction,
-                                     death_rate=self.death_rate, trajectories=self.trajectories,
-                                     n_stop=self.n_stop)
-        bnab_sequential.run(reps=self.num_reps)
+        bnab_prime = Simulation(self.p_ini, self.parameters, trajectories=self.trajectories)
+        bnab_prime.run(reps=self.num_reps)
 
     def run_sequential(self):
 
@@ -121,15 +163,10 @@ class ProcedureDelS1S2(Procedure):
         else:
             np.savetxt("total_bnabs", np.zeros(len(self.sigma[1:])), fmt='%f')
 
-
-class ProcedureIndividualRestart(ProcedureDelS1S2):
-
-    def __init__(self, sigma_1=10.0, death_rate=0.1, fraction=7.0/8.0, trajectories=False):
-        ProcedureDelS1S2.__init__(self, sigma_1=sigma_1, death_rate=death_rate, fraction=fraction,
-                                  trajectories=trajectories)
-
     def next_injection(self, index, sigma):
         make_and_cd("injection_{0}_sig_{1}".format(index, round(sigma, 2)))
+
+        self.parameters['sigma'] = sigma
         success_exit = np.loadtxt("../successful_exit")
 
         if len(success_exit.shape) == 1:
@@ -149,25 +186,27 @@ class ProcedureIndividualRestart(ProcedureDelS1S2):
 
             p_ini = n_i_exit[1:]/np.sum(n_i_exit[1:])
 
-            if len(self.p_ini) > 7:
-                gillespie_bnab = BnabFiniteSizeEffects(p_ini, fraction=self.fraction, sigma=sigma,
-                                                            death_rate=self.death_rate,
-                                                            trajectories=self.trajectories, n_stop=self.n_stop)
-            else:
-                gillespie_bnab = BnabGillespie(p_ini, fraction=self.fraction, sigma=sigma,
-                                               death_rate=self.death_rate, trajectories=self.trajectories,
-                                               n_stop=self.n_stop)
+            bnab_next_injection = Simulation(self.p_ini, self.parameters, trajectories=self.trajectories)
 
-            gillespie_bnab.define_tmat()
+            # if len(self.p_ini) > 7:
+            #     gillespie_bnab = BnabFiniteSizeEffects(p_ini, fraction=self.fraction, sigma=sigma,
+            #                                            death_rate=self.death_rate, n_stop=self.n_stop)
+            # else:
+            #     gillespie_bnab = BnabGillespie(p_ini, fraction=self.fraction, sigma=sigma, death_rate=self.death_rate,
+            #                                    n_stop=self.n_stop)
+            #
+            # gillespie_bnab.define_tmat()
 
             if self.trajectories:
-                M = ModelMFPTTrajectories(p_ini=p_ini, vnames=gillespie_bnab.vars, tmat=gillespie_bnab.tmat,
-                                          propensity=gillespie_bnab.prop, n_exit=n_i_exit[1:])
+                M = ModelMFPTTrajectories(p_ini=p_ini, vnames=bnab_next_injection.gillespie_bnab.vars,
+                                          tmat=bnab_next_injection.gillespie_bnab.tmat,
+                                          propensity=bnab_next_injection.gillespie_bnab.prop, n_exit=n_i_exit[1:])
 
             else:
-                M = ModelIndividualGC(n_exit=n_i_exit[1:], p_ini=p_ini, vnames=gillespie_bnab.vars,
-                                      tmat=gillespie_bnab.tmat, propensity=gillespie_bnab.prop,
-                                      n_stop=gillespie_bnab.n_stop)
+                M = ModelIndividualGC(n_exit=n_i_exit[1:], p_ini=p_ini, vnames=bnab_next_injection.gillespie_bnab.vars,
+                                      tmat=bnab_next_injection.gillespie_bnab.tmat,
+                                      propensity=bnab_next_injection.gillespie_bnab.prop,
+                                      n_stop=self.parameters['n_stop'])
 
             M.run(tmax=1000, reps=count)
             # tc.append(M.t_exit)
@@ -175,12 +214,13 @@ class ProcedureIndividualRestart(ProcedureDelS1S2):
             count += 1
 
         # np.savetxt("tc_gc_exit", tc, fmt='%f')
-        np.savetxt("fitness", gillespie_bnab.bnab.fitness.f, fmt='%f')
-        np.savetxt("sigma", [gillespie_bnab.bnab.fitness.sigma], fmt='%f')
-        np.savetxt("death_rate", [gillespie_bnab.bnab.mu_i0], fmt='%f')
-        np.savetxt("mutation_rate", [gillespie_bnab.bnab.mu_ij], fmt='%f')
-        np.savetxt("fraction", [gillespie_bnab.bnab.fraction], fmt='%f')
+        np.savetxt("fitness", bnab_next_injection.gillespie_bnab.bnab.fitness.f, fmt='%f')
+        np.savetxt("sigma", [bnab_next_injection.gillespie_bnab.bnab.fitness.sigma], fmt='%f')
+        np.savetxt("death_rate", [bnab_next_injection.gillespie_bnab.bnab.mu_i0], fmt='%f')
+        np.savetxt("mutation_rate", [bnab_next_injection.gillespie_bnab.bnab.mu_ij], fmt='%f')
+        np.savetxt("fraction", [bnab_next_injection.gillespie_bnab.bnab.fraction], fmt='%f')
 
         print("Starting post-processing...")
-        post_process = GillespieGCExit(n_exit=self.n_stop, num_odes=gillespie_bnab.len_ini)
+        post_process = GillespieGCExit(n_exit=self.parameters['n_stop'],
+                                       num_odes=bnab_next_injection.gillespie_bnab.len_ini)
         post_process.populate_pn(index=1)
