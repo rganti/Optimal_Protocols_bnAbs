0d0008583dd689985dbe2a5ad97f801c49a88d1d
ramanganti <rg468@cam.ac.uk>
2020-02-10 16:21:57 -0500

    "version used to generate data/raw/extended_breadth_multiple_trials/Trials_dr_0.1_muij_0.05_bins_7"

--- src/data/generate_protocol_curves.py
+++ src/data/generate_protocol_curves.py
@@ -42,6 +42,16 @@ class SlurmProtocolCurves(object):
         q.close()
 
 
+def define_n_initial(num_bins):
+    n_initial = np.zeros(num_bins, dtype=float)
+    n_initial[0] = 20.0
+    n_initial[1] = 5.0
+    n_initial[-1] = 20.0
+    n_initial[-2] = 5.0
+
+    return n_initial
+
+
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Submitting immune protocol calculations.",
                                      formatter_class=argparse.ArgumentDefaultsHelpFormatter)
@@ -51,37 +61,42 @@ if __name__ == "__main__":
 
     args = parser.parse_args()
 
-    parameters = { #'n_initial': [20.0, 5.0, 0.0, 0.0, 0.0, 0.0, 0.0, 5.0, 20.0],
-                  'n_initial': [20.0, 5.0, 0.0, 0.0, 0.0, 5.0, 20.0],
-                  'sigma': args.sigma1,
-                  'death_rate': 0.05, 'fraction': 7.0 / 8.0, 'mutation_rate': 0.05, 'n_stop': 200}
+    parameters = {'n_initial': define_n_initial(7), 'sigma': args.sigma1, 'death_rate': 0.1,
+                  'fraction': 7.0 / 8.0, 'mutation_rate': 0.05, 'n_stop': 200}
 
     if args.sigma1:
         protocol = ProcedureDelS1S2(parameters)
         protocol.run_sequential()
         # protocol.run_prime()
     else:
-        make_and_cd("test_dr_{0}_muij_{1}_bins_{2}".format(parameters['death_rate'], parameters['mutation_rate'],
-                                                           len(parameters['n_initial'])))
-        home = os.getcwd()
-        sigma_1_range = np.logspace(1.0, -1.0, num=15)
-        # # sigma_1_range = np.logspace(0.6, 0.0, num=20)
-        # # sigma_1_range = [0.75, 0.8, 0.85, 0.9, 0.95]
+        make_and_cd("Trials_dr_{0}_muij_{1}_bins_{2}".format(parameters['death_rate'], parameters['mutation_rate'],
+                                                             len(parameters['n_initial'])))
 
-        # sigma_1_range = [1.0, 1.2, 1.3, 1.7, 2.0]
+        home = os.getcwd()
+
+        # sigma_1_range = np.logspace(1.0, -1.0, num=15)
+        # # sigma_1_range = np.logspace(0.6, 0.0, num=20)
+        sigma_1_range = [0.7, 1.0, 1.3, 1.7, 1.93, 2.1, 2.3, 3.0]
         np.savetxt("sigma_1_range", sigma_1_range[::-1], fmt='%f')
 
         for sigma1 in sigma_1_range:
             make_and_cd("Sigma_{0}".format(round(sigma1, 2)))
-            # np.savetxt("fitness", gillespie_bnab.bnab.fitness.f, fmt='%f')
 
-            if socket.gethostname() == "eofe4.mit.edu":
-                sbatch = SlurmProtocolCurves(sigma_1=sigma1)
-                sbatch.generate_sbatch()
-                run_sbatch()
-            else:
-                qsub = QsubProtocolCurves(sigma_1=sigma1)
-                qsub.generate_qsub()
-                run_qsub()
+            sigma_directory = os.getcwd()
+
+            for t in range(30):
+                make_and_cd("Trial_{0}".format(t))
+
+                if socket.gethostname() == "eofe4.mit.edu":
+                    sbatch = SlurmProtocolCurves(sigma_1=sigma1)
+                    sbatch.generate_sbatch()
+                    run_sbatch()
+                else:
+                    qsub = QsubProtocolCurves(sigma_1=sigma1)
+                    qsub.generate_qsub()
+                    run_qsub()
+
+                os.chdir(sigma_directory)
 
             os.chdir(home)
+
