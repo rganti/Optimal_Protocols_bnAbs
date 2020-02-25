74d15302198eba572f99f954bedad416f8c06060
ramanganti <rg468@cam.ac.uk>
2020-02-24 20:42:55 -0500

    "script for generating protocol curves."

--- src/data/generate_protocol_curves.py
+++ src/data/generate_protocol_curves.py
@@ -4,16 +4,17 @@ the protocol curves showing bnabs/gc produced versus KL distance of p1 and f2.''
 import argparse
 import os
 import socket
+import time
 
 import numpy as np
 
-from src.data.procedures import ProcedureDelS1S2
+from src.data.procedures import ProcedureDelS1S2, define_n_initial
 from src.general.directory_handling import make_and_cd
 from src.general.queuing import QsubHeader, SlurmHeader, run_sbatch, run_qsub
 
 
 class QsubProtocolCurves(object):
-    def __init__(self, sigma_1, simulation_time=10, nodes=1, ppn=1):
+    def __init__(self, sigma_1, simulation_time=30, nodes=1, ppn=1):
         self.sigma_1 = sigma_1
         self.header = QsubHeader(simulation_name="protocols_{0}".format(round(self.sigma_1, 2)),
                                  simulation_time=simulation_time, nodes=nodes, ppn=ppn)
@@ -30,7 +31,7 @@ class QsubProtocolCurves(object):
 
 
 class SlurmProtocolCurves(object):
-    def __init__(self, sigma_1, simulation_time=10, nodes=1, ppn=1):
+    def __init__(self, sigma_1, simulation_time=30, nodes=1, ppn=1):
         self.sigma_1 = sigma_1
         self.header = SlurmHeader(simulation_name="optimal_curves", simulation_time=simulation_time, nodes=nodes,
                                   ppn=ppn)
@@ -44,16 +45,6 @@ class SlurmProtocolCurves(object):
         self.header.set_header(q)
         self.set_python_script(q)
         q.close()
-
-
-def define_n_initial(num_bins):
-    n_initial = np.zeros(num_bins, dtype=float)
-    n_initial[0] = 20.0
-    n_initial[1] = 5.0
-    n_initial[-1] = 20.0
-    n_initial[-2] = 5.0
-
-    return n_initial
 
 
 if __name__ == "__main__":
@@ -70,8 +61,8 @@ if __name__ == "__main__":
 
     if args.sigma1:
         protocol = ProcedureDelS1S2(parameters)
-        protocol.run_sequential()
-        # protocol.run_prime()
+        # protocol.run_sequential()
+        protocol.run_prime()
     else:
         make_and_cd("Trials_dr_{0}_muij_{1}_bins_{2}".format(parameters['death_rate'], parameters['mutation_rate'],
                                                              len(parameters['n_initial'])))
@@ -80,7 +71,7 @@ if __name__ == "__main__":
 
         # sigma_1_range = np.logspace(1.0, -1.0, num=15)
         # # sigma_1_range = np.logspace(0.6, 0.0, num=20)
-        sigma_1_range = [0.8, 1.0, 1.2, 1.3, 1.5]
+        sigma_1_range = [0.8, 0.9, 1.0, 1.1, 1.2, 1.3, 1.4, 1.5]
         np.savetxt("sigma_1_range", sigma_1_range[::-1], fmt='%f')
 
         for sigma1 in sigma_1_range:
@@ -91,7 +82,7 @@ if __name__ == "__main__":
             for t in range(30):
                 make_and_cd("Trial_{0}".format(t))
 
-                if socket.gethostname() == "eofe4.mit.edu":
+                if socket.gethostname() == "eofe4.mit.edu" or socket.gethostname() == "eofe7.cm.cluster":
                     sbatch = SlurmProtocolCurves(sigma_1=sigma1)
                     sbatch.generate_sbatch()
                     run_sbatch()
