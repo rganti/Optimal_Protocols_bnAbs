97a3819b16244c1ed77871aff7e59be0c4b846ea
rg468 <rg468@cam.ac.uk>
2020-06-02 14:21:55 -0400

    remaining files.

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='The project aims to use a 1 dimensional fitness landscape in a breadth space to determine which '
+                'injection protocols maximize bnAb production  and whfeatures of the optimal protocols.',
+    author='Raman Ganti',
+    license='MIT',
+)
