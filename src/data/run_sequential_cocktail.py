f4d916e325425044826f2e18acc53707128901d9
rg468 <rg468@cam.ac.uk>
2020-06-02 13:48:27 -0400

    "Changed parameters for running sequential immunization simulations."

--- src/data/run_sequential_cocktail.py
+++ src/data/run_sequential_cocktail.py
@@ -3,7 +3,7 @@ from src.data.procedures import Procedure, define_n_initial
 if __name__ == "__main__":
 
     parameters = {'n_initial': define_n_initial(15), 'death_rate': 0.02, 'fraction': 7.0 / 8.0, 'mutation_rate': 0.05,
-                  'n_stop': 200, 'sigma': 1.3, 'sigma2': 1.0, 'sigma3': 0.8}
+                  'n_stop': 200, 'sigma': 1.2, 'sigma2': 0.7, 'sigma3': 0.3}
 
     protocol = Procedure(parameters)
-    protocol.run_cocktail()
+    protocol.run_sequential()
