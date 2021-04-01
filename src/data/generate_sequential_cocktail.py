850fac1a92226305178e41b175b8a92352be8a6b
rg468 <rg468@cam.ac.uk>
2021-04-01 12:43:50 -0400

    "refactored and moved define_n_initial"

--- src/data/generate_sequential_cocktail.py
+++ src/data/generate_sequential_cocktail.py
@@ -1,7 +1,7 @@
-from src.data.procedures import Procedure, define_n_initial
+from src.data.gillespie_models import define_n_initial
+from src.data.procedures import Procedure
 
 if __name__ == "__main__":
-
     parameters = {'n_initial': define_n_initial(15), 'death_rate': 0.02, 'fraction': 7.0 / 8.0, 'mutation_rate': 0.05,
                   'n_stop': 200, 'sigma': 1.2, 'sigma2': 0.7, 'sigma3': 0.3}
 
